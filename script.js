33c559b7e9807999453cff1977aec31b65e7beef
kneves <55398273+kneves1@users.noreply.github.com>
2020-02-16 22:20:15 -0500

    On-click event starts 60 sec timer (timer unfortunately goes to neg infinity)

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//On click event to 
+//On click event on "Start Quiz" button
+
+//
+
+
+let score = 0;
+let seconds = 60;
+let interval = 1000;
+
+//Timer
+$(".btn").on("click", function timer(){
+        timer = setInterval(function(){
+        $(".timer").text("Seconds: " + seconds);
+        seconds--;
+        
+        if (seconds < 0){
+            clearInterval(timer);
+        }  
+
+        // if(answer = incorrect){ //haven't declared these variables
+        //  let penalty = (seconds * 5)
+        // } 
+
+    }, interval);   
+});
+timer()
